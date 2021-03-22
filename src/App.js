3a21bdece6ecfc27f12e16a5692b0fb07a01cae6
Houssem Bougassas <Bougassas.h@gmail.com>
2021-03-22 20:35:01 +0100

    Props

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import ProfileComponent from "./Profile/ProfileComponent";
+
+function App() {
+    const handleName = (fullName) => {
+        alert(`My name is ${fullName}`);
+    };
+    return (
+        <ProfileComponent
+            fullName="Foulen Ben Foulen"
+            handleName={handleName}
+            bio="Welcome to my BIO"
+            profession="Student"
+        >
+            <img
+                style={{
+                    borderRadius: "20px",
+                    width: "500px",
+                    height: "300px",
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+                src="https://www.sport.fr/wp-content/uploads/2021/02/Icon_PS_210131_041.jpg"
+                alt="pic"
+            />
+        </ProfileComponent>
+    );
+}
+
+export default App;
